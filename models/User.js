33a32ac61fc0757864574d164c28c077450f4bc7
Lesly <leslyr562@gmail.com>
2021-12-25 19:27:52 -0600

    finish models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            required: 'Email address is required',
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, User) => total + friends.length + 1, 0);
+});
+
+// export the Pizza model
+module.exports = User;
